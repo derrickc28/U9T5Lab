23ba08ab13808446cb16d14af47cc38c94a476ab
Derrick Chen <derrickc28@nycstudents.net>
2022-03-30 18:34:49 -0400

    ooga

--- src/Cabaret.java
+++ src/Cabaret.java
@@ -29,4 +29,72 @@ public class Cabaret {
         return false;
     }
 
+    public boolean removePerformer(Performer p)
+    {
+        int index = performers.indexOf(p);
+        if(index != -1)
+        {
+            performers.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public double averagePerformerAge()
+    {
+        int ageSum = 0;
+        for(Performer p : performers)
+        {
+            ageSum += p.getAge();
+        }
+        double average = ageSum / (double) performers.size();
+        return average;
+    }
+
+    public ArrayList<Performer> performersOverAge(int age)
+    {
+        ArrayList<Performer> list = new ArrayList<Performer>();
+        for(Performer p : performers)
+        {
+            if(p.getAge() >= age)
+            {
+                list.add(p);
+            }
+        }
+        return list;
+    }
+
+    public void groupRehearsal()
+    {
+        for(Performer p : performers)
+        {
+            System.out.println("REHEARSAL CALL! " + p.getName());
+
+            if(p instanceof Comedian)
+            {
+                Comedian c = (Comedian) p;
+                c.rehearse(false);
+            }
+            else
+            {
+                p.rehearse();
+            }
+        }
+    }
+
+    public void CabaretShow()
+    {
+        for(Performer p : performers)
+        {
+            System.out.println("Welcome to the cabaret! Next act up... " + p.getName());
+
+            if(p instanceof Dancer)
+            {
+                Dancer d = (Dancer) p;
+                d.pirouette(2);
+            }
+            p.perform();
+        }
+    }
+
 }
